f2bc5016d2a0d539744e2f0e522522c7f8b766d9
CletsyMedia <Cletussam12@yahoo.com>
2024-05-21 23:10:46 +0100

    MRU (Most Recently Used) caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+MRU Cache module
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache inherits from BaseCaching and is a caching system.
+    """
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                del self.cache_data[key]
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key = next(iter(self.cache_data))
+                del self.cache_data[mru_key]
+                print("DISCARD: {}".format(mru_key))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
+
+
+if __name__ == '__main__':
+    my_cache = MRUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
