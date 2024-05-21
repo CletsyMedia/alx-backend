d8cef6309cce5a7cf3c5cced99ea061546c277cf
CletsyMedia <Cletussam12@yahoo.com>
2024-05-21 23:13:24 +0100

    MRU (Most Recently Used) caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,64 +1,49 @@
 #!/usr/bin/env python3
+"""Create a class MRUCache that inherits
+from BaseCaching and is a caching system:
+
 """
-MRU Cache module
-"""
-from base_caching import BaseCaching
+
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """
-    MRUCache inherits from BaseCaching and is a caching system.
+    """_summary_
     """
 
     def __init__(self):
-        """ Initialize
+        """_summary_
         """
         super().__init__()
+        self.usedKeys = []
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """_summary_
+
+        Args:
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
         if key is not None and item is not None:
-            if key in self.cache_data:
-                del self.cache_data[key]
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                mru_key = next(iter(self.cache_data))
-                del self.cache_data[mru_key]
-                print("DISCARD: {}".format(mru_key))
             self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """ Get an item by key
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
         """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
         return None
-
-
-if __name__ == '__main__':
-    my_cache = MRUCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    print(my_cache.get("B"))
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
-    print(my_cache.get("B"))
-    print(my_cache.get("C"))
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
-    my_cache.put("G", "San Francisco")
-    my_cache.print_cache()
-    my_cache.put("H", "H")
-    my_cache.print_cache()
-    my_cache.put("I", "I")
-    my_cache.print_cache()
-    my_cache.put("J", "J")
-    my_cache.print_cache()
-    my_cache.put("K", "K")
-    my_cache.print_cache()
