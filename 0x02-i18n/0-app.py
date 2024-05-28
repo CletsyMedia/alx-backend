df63dad7873a0f17c8369cc3075417c760eb1b7f
CletsyMedia <Cletussam12@yahoo.com>
2024-05-28 17:16:34 +0100

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
-"""A simple flask app
 """
-
-
+0-app module
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    """_summary_
+def index():
     """
-    return render_template('0-index.html')
+    Index route
+    """
+    return render_template(
+        '0-index.html',
+        title="Welcome to Holberton",
+        header="Hello world")
 
 
-if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
